65e80e8c9a2113739b7b2b636ce90a40866a73a5
= <=>
2022-10-06 16:10:21 -0500

    fix

--- components/Select/index.jsx
+++ components/Select/index.jsx
@@ -18,7 +18,7 @@ export default function Select() {
       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:slate-300 focus:border-slate-300 block w-72 p-2.5 dark:bg-slate-800 dark:border-slate-300 dark:placeholder-slate-300 dark:text-white dark:focus:ring-slate-300 dark:focus:border-slate-300"
       onChange={onChangeURL}
     >
-      <option onClick={backToHome}  value="./">Todas</option>
+      <option onClick={backToHome}  value="./index">Todas</option>
       <option value="xiaomi">xiaomi</option>
       <option value="samsung">Samsung</option>
       <option value="apple">Apple</option>
